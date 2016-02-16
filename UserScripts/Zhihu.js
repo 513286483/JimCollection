1e2e5b189fd023f933ea3e67836f354ddd2fe22f
Jim <jimzuolin@gmail.com>
2016-02-16 10:14:21 +0000

    bug fix

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -1,5 +1,5 @@
 'use strict';
-
+debugger;
 $(() => {
     var sign = 'link.zhihu.com/?target=';
     var links = $('a.external');
@@ -8,7 +8,7 @@ $(() => {
         link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
     }
 
-    $('<style>@media screen and(max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('head');
+    $('<style>@media screen and (max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('head');
 });
 
 $(document).on('copy', event => {
