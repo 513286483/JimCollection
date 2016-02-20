4c8ca5e278186a2fb159427a5b91584efab0b2ed
Jim <jimzuolin@gmail.com>
2016-02-20 21:52:23 +0000

    use JS Style Event

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -1,18 +1,16 @@
 'use strict';
 
-$(() => {
-    var sign = 'link.zhihu.com/?target=';
-    var links = $('a.external');
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
-    }
-
-    $('<style>@media screen and (max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('head');
-});
-
-$(document).on('copy', event => {
-    event.preventDefault();
-    event.stopPropagation();
-    event.originalEvent.clipboardData.setData('text/plain', getSelection().toString());
-});
+$('<style>@media screen and (max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('head');
+$(document)
+    .on('copy', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        event.originalEvent.clipboardData.setData('text/plain', getSelection().toString());
+    })
+    .on('click', event => {
+        var sign = 'link.zhihu.com/?target=';
+        var link = event.target;
+        if (link.classList.contains('external')) {
+            link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
+        }
+    });
