73c218e6e435939d345a2ff88f1abf8a7155e2a7
Jim <jimzuolin@gmail.com>
2016-02-22 20:38:06 +0000

    better loop

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -8,9 +8,9 @@ $(document)
         event.originalEvent.clipboardData.setData('text/plain', getSelection().toString());
     })
     .on('click', event => {
+        const sign = 'link.zhihu.com/?target=';
         var link = event.target;
         if (link.classList.contains('external')) {
-            const sign = 'link.zhihu.com/?target=';
             link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
         }
     });
