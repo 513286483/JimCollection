79e77874aead4861a137f2f5143fa7c3d9924722
Jim <jimzuolin@gmail.com>
2016-02-25 00:12:04 +0000

    crack Zhihu copy and paste

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -2,15 +2,13 @@
 
 $('<style>@media screen and (max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('html');
 $(document)
-    .on('copy', event => {
-        event.preventDefault();
-        event.stopPropagation();
-        event.originalEvent.clipboardData.setData('text/plain', getSelection().toString());
+    .on('copy', () => {
+        GM_setClipboard(getSelection().toString(), 'text');
     })
     .on('click', event => {
-        const sign = 'link.zhihu.com/?target=';
         var link = event.target;
         if (link.classList.contains('external')) {
+            const sign = 'link.zhihu.com/?target=';
             link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
         }
     });
