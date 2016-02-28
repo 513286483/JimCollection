32f5e8a32f1280b31b422690c1de4a089914c578
Jim <jimzuolin@gmail.com>
2016-02-28 12:05:15 +0000

    improve

--- UserScripts/NewsWY.js
+++ UserScripts/NewsWY.js
@@ -2,9 +2,8 @@
 
 $(() => {
     $('iframe').remove();
-    var node;
     if (!location.href.includes('comment')) {
-        node = $('.ep-content-main');
+        var node = $('.ep-content-main');
         node.add(node.parentsUntil(document.body)).siblings().hide();
         node.find('.ep-share-tip, .ep-share-top, .ep-icon-tie')
             .add(node.find('.end-text:last').nextAll())
