7e9f2092fb9c33084564bef71b8cd05e2cf26bd6
Jim <jimzuolin@gmail.com>
2016-03-17 21:22:03 +0000

    merge scripts

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -20,20 +20,22 @@ $(document)
     });
 
 if (location.pathname === '/topic') {
-    var topics = $('.topic-feed-item > a');
-    for (var i = 0; i < topics.length; i++) {
-        var topic = topics[i];
-        (topic => {
-            $.get(topic.href, data => {
+    $(() => {
+        var topics = $('.topic-feed-item > a');
+        for (var i = 0; i < topics.length; i++) {
+            var topic = topics[i];
+            (topic => {
+                $.get(topic.href, data => {
 
-                var page = $(data.replace(/<img[^>]*>/g, ''));
-                var counts = page.find('.count');
-                counts.sort((a, b) => -(a.innerText - b.innerText));
+                    var page = $(data.replace(/<img[^>]*>/g, ''));
+                    var counts = page.find('.count');
+                    counts.sort((a, b) => -(a.innerText - b.innerText));
 
-                counts.length > 0 ?
-                    topic.innerText = topic.innerText + ' ' + counts.length + ':' + counts.first().text() :
-                    topic.innerText += ' ✓';
-            });
-        })(topic);
-    }
+                    counts.length > 0 ?
+                        topic.innerText = topic.innerText + ' ' + counts.length + ':' + counts.first().text() :
+                        topic.innerText += ' ✓';
+                });
+            })(topic);
+        }
+    });
 }
