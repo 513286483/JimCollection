6b0fc201352b7100ce1a4963af97095a0159c150
Jim <jimzuolin@gmail.com>
2016-03-17 00:04:50 +0000

    better detection

--- UserScripts/Vim.js
+++ UserScripts/Vim.js
@@ -141,7 +141,8 @@ var Page = {
 
                         for (i = 0; i < positions.length; i++) {
                             var targetElement = document.elementFromPoint(positions[i][0], positions[i][1]);
-                            if (targetElement === element || element.contains(targetElement)) {
+                            if (targetElement === element ||
+                                element.contains(targetElement) || targetElement.contains(element)) {
                                 return true;
                             }
                         }
